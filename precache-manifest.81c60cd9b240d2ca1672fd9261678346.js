c307d0c9492296a7336f0a1af495597e70934e59
Maciej Durda <maciej.durda.1995@gmail.com>
2020-06-17 23:28:45 +0200

    Updates

---
+++ precache-manifest.81c60cd9b240d2ca1672fd9261678346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3de7009af8c0287922a0a3f4fe0905d",
+    "url": "/monsters-project-course/index.html"
+  },
+  {
+    "revision": "c31b1ef6796e46f94e16",
+    "url": "/monsters-project-course/static/css/main.ae876e1c.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-project-course/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-project-course/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c31b1ef6796e46f94e16",
+    "url": "/monsters-project-course/static/js/main.1d344242.chunk.js"
+  },
+  {
+    "revision": "507efb0c26c9e37d1857",
+    "url": "/monsters-project-course/static/js/runtime-main.8df050f1.js"
+  }
+]);
